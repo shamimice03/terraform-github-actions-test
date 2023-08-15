0c197ce9c22cff87ebc4e352dae907616d76956e
shamimice03 <shamimice03@gmail.com>
2023-08-15 09:06:05 +0000

    fix(variable-data): fixed variable default value

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "enable_single_nat_gateway" {
 variable "azs" {
   description = "List of AZs"
   type        = list(any)
-  default     = ["ap-northeast-1a"]
+  default     = []
 }
 
 variable "public_subnet_cidr" {
